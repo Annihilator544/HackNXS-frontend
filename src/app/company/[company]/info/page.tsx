ee4b68ce1176a8d9b7dd2b6a602636bd1c75114c
Chandan <chandancvp08@gmail.com>
2024-04-07 09:17:37 +0530

    done merging and shit

--- src/app/company/[company]/info/page.tsx
+++ src/app/company/[company]/info/page.tsx
@@ -1,28 +1,47 @@
-'use client'
+"use client";
 import { useParams } from "next/navigation";
-import { useQuery } from 'react-query';
+import { useQuery } from "react-query";
 import { useGlobalAppState } from "@/app/use-global-app-state";
+import Header from "@/components/Header";
+import ReactApexChart from "react-apexcharts";
 
-function Info(){
-    useGlobalAppState.setState({ loadingProgress: 0 })
-        const { company }: {company : string} = useParams();
-        const { data  } = useQuery(['company', company], async ()=>{
-            useGlobalAppState.setState({ loadingProgress: 50 })
-            const response = await fetch(`http://127.0.0.1:5000/getsentiment/${company}`);
-            const data = await response.json();
-            return data;
-        })
-        useGlobalAppState.setState({ loadingProgress: 100 })
-    return(<>
-        {data && (
-                <div>
-                    <h1>{data['positive']}</h1>
-                    <h2>{data['negative']}</h2>
-                    <h3>{data['neutral']}</h3>
-                </div>
-            )
+function Info() {
+  useGlobalAppState.setState({ loadingProgress: 0 });
+  const { company }: { company: string } = useParams();
+  const { data } = useQuery(["company", company], async () => {
+    useGlobalAppState.setState({ loadingProgress: 50 });
+    const response = await fetch(
+      `http://127.0.0.1:5000/getsentiment/${company}`
+    );
+    const data = await response.json();
+    return data;
+  });
+  useGlobalAppState.setState({ loadingProgress: 100 });
+
+  const options= {
+    labels : ["Positive", "Negative", "Neutral" ],
+    colors:["#77DD77", "#FF6961", "#AEC6CF"]
+  }
+
+  return (
+    <div>
+      <Header />
+      <div className="flex w-full justify-center items-center">
+      <div id="chart">
+        {
+
+        data?   <ReactApexChart
+            options={options}
+            series={[data['positive'],data['negative'],data['neutral']]}
+            type="pie"
+            width={500}
+            />:null
         }
-    </>)
+      </div>
+      <div id="html-dist"></div>
+    </div>
+    </div>
+  );
 }
 
-export default Info
+export default Info;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,63 +1,91 @@
-'use client'
-import Navbar from "@/components/Navbar"
+"use client";
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 import ApexCharts from "@/components/ui/ApexCharts";
 import ApexAreaChart from "@/components/ui/ApexCharts";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 
-function Dashboard(){
-    const companies = ['INFY', 'AAPL', 'MSFT','TCS','GOOGL','AMZN','FB','WIPRO','HCLTECH','NSE: RELIANCE'];
-    const [companyData, setCompanyData] = useState<any[]>([]);
+function Dashboard() {
+  const companies = [
+    "INFY",
+    "AAPL",
+    "MSFT",
+    "TCS",
+    "GOOGL",
+    "AMZN",
+    "FB",
+    "WIPRO",
+    "HCLTECH",
+    "NSE: RELIANCE",
+  ];
+  const [companyData, setCompanyData] = useState<any[]>([]);
 
-    const fetchData = async (company: string) => {
-        const response = await fetch(`http://127.0.0.1:5000/gethistoricaldata/${company}`);
-        const data = await response.json();
-        setCompanyData((prevData) => [...prevData, data]);
-    };
+  const fetchData = async (company: string) => {
+    const response = await fetch(
+      `http://127.0.0.1:5000/gethistoricaldata/${company}`
+    );
+    const data = await response.json();
+    setCompanyData((prevData) => [...prevData, data]);
+  };
 
-    useEffect(() => {
-        companies.forEach((company) => {
-            fetchData(company);
-        });
-    }, []);
-    return(
-        <>
-        <Navbar/>
-        <Table>
-            <TableHeader >
-                <TableRow >
-                    <TableHead>
-                        Stock name
-                    </TableHead>
-                    <TableHead>
-                        Recent Data
-                    </TableHead>
-                    <TableHead>
-                        Base Price
-                    </TableHead>
-                    <TableHead>
-                        Last Price
-                    </TableHead>
-                    <TableHead>
-                        Percentage change
-                    </TableHead>
+  useEffect(() => {
+    companies.forEach((company) => {
+      fetchData(company);
+    });
+  }, []);
+  return (
+    <>
+      <Header />{" "}
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Stock name</TableHead>
+            <TableHead>Recent Data</TableHead>
+            <TableHead>Base Price</TableHead>
+            <TableHead>Last Price</TableHead>
+            <TableHead>Percentage change</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {companyData
+            .filter(
+              (company, index, self) =>
+                self.findIndex((c) => c.stockName === company.stockName) ===
+                index
+            )
+            .map((c) => {
+              return (
+                <TableRow
+                  key={c.stockName}
+                  onClick={() => {
+                    window.location.pathname = `/company/${c.stockName.replaceAll(
+                      /\s/g,
+                      "_"
+                    )}/info`;
+                  }}
+                >
+                  <TableCell>{c.stockName}</TableCell>
+                  <TableCell>
+                    <ApexCharts xaxis={c.date} series={c.closePrice} />
+                  </TableCell>
+                  <TableCell>{c.basePrice}</TableCell>
+                  <TableCell>{c.lastPrice}</TableCell>
+                  <TableCell>{c.pChange}</TableCell>
                 </TableRow>
-            </TableHeader>
-            <TableBody>
-                {companyData.filter((company, index, self) => self.findIndex(c => c.stockName === company.stockName) === index).map((company)=>{
-                    return(
-                    <TableRow key={company.stockName}>
-                        <TableCell>{company.stockName}</TableCell>
-                        <TableCell><ApexCharts xaxis={company.date} series={company.closePrice}/></TableCell>
-                        <TableCell>{company.basePrice}</TableCell>
-                        <TableCell>{company.lastPrice}</TableCell>
-                        <TableCell>{company.pChange}</TableCell>
-                    </TableRow>)
-                })}
-            </TableBody>
-        </Table>
-        </>
-    )
+              );
+            })}
+        </TableBody>
+      </Table>
+    </>
+  );
 }
-export default Dashboard
+export default Dashboard;
